93a027ccd976122a353c183b083ded624588bfcc
poxonpea <terripoxonpearson@gmail.com>
2017-01-19 16:47:54 -0500

    Updated File
    Updated in Computer Lab

--- Project1/SOURCE/GaussianElimination.f90
+++ Project1/SOURCE/GaussianElimination.f90
@@ -1,16 +1,40 @@
 program GaussElimination
 	implicit none
+	! Declare Variables
 	real(8)::SourceValue
-	real(8)::Fvalue,x,StepSize
+	real(8)::Fvalue,x,StepSize,x0,e
 	integer, parameter :: NumGridPoints = 10
+	real(8), Dimension(NumGridPoints+1) :: u,d,f
+	integer:: i
 
-	StepSize = 1.d0/NumGridPoints
-	
+	!Initialize Variables
+	StepSize = 1.d0/(NumGridPoints)
+	u(1)=0
+	u(NumGridPoints+1)=0
+	d(1)=2
+	d(NumGridPoints+1)=2
+	e=-1.d0
+	do i=1,NumGridPoints+1
+		f(i)=SourceValue((i-1)*StepSize)*StepSize**2.d0
+		print *, (i-1)*StepSize, f(i)
+	end do
 
+	!Forward Substitution
+	do i=2, NumGridPoints
+		d(i)=d(i) - (e * e)/ d(i-1)
+		f(i)=f(i) - (e * f(i-1))/ d(i-1)
+	end do
 
-!	x=2.d0
-!	Fvalue = EquationValue(x)
-!	print*, Fvalue
+	!Backward Substitution
+	u(NumGridPoints) = f(NumGridPoints)/d(NumGridPoints)
+
+	do i=NumGridPoints-1, 2, -1
+		u(i)= (f(i) - e * u(i+1) )/ d(i)
+	end do
+
+	do i=1,NumGridPoints+1
+		print *, (i-1)*StepSize, u(i)
+	end do
 
 
 end program GaussElimination
@@ -19,7 +43,7 @@ function SourceValue(x)
 	implicit none
 	real(8)::SourceValue, x
 
-	SourceValue = 100.d0 * Exp(10.d0 * x)
+	SourceValue = 100.d0 * Exp(- 10.d0 * x)
 	return
 
-end function EquationValue	
+end function SourceValue	

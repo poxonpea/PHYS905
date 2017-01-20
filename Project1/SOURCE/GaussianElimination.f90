07c8efe1b9a2089e073a3e9cdec6f3958c55af8a
poxonpea <poxonpea@msu.edu>
2017-01-20 16:35:56 -0500

    Updated

--- Project1/SOURCE/GaussianElimination.f90
+++ Project1/SOURCE/GaussianElimination.f90
@@ -2,30 +2,39 @@ program GaussElimination
   implicit none
   ! Declare Variables
   real(8)::SourceValue
-  real(8)::x,StepSize,x0,e
-  integer, parameter :: NumGridPoints = 100
-  real(8), Dimension(NumGridPoints+1) :: u,d,f
+  real(8)::StepSize
+  integer:: NumGridPoints,Power
+  real(8), Dimension(:),Allocatable :: u,d,f,e
   integer:: i
 
+  !Ask user for sizes of matrix and allocate Matricies
+  print*,"What is the maximum power (base 10) of the matrix size you want?"
+  read (*,*) Power
+
+  NumGridPoints=10**Power
+
+  print*,NumGridPoints
+  
+  Allocate(u(NumGridPoints+1))
+  Allocate(d(NumGridPoints+1))
+  Allocate(f(NumGridPoints+1))
+  Allocate(e(NumGridPoints+1))
+  
   !Initialize Variables
   StepSize = 1.d0/(NumGridPoints)
   u(1)=0.d0
   u(NumGridPoints+1)=0.d0
-  !d(2)=2.d0
-  !d(NumGridPoints)=2.d0
-  e=-1.d0
   do i=1,NumGridPoints+1
      f(i)=SourceValue((i-1)*StepSize)*StepSize**2.d0
      d(i)=2.d0
-     
-     !print *, (i-1)*StepSize, f(i)
+     e(i)=-1.d0
   end do
 
   !Forward Substitution
   do i=3, NumGridPoints
-     d(i)= d(i) - (e * e)/ d(i-1)
+     d(i)= d(i) - (e(i-1) * e(i-1))/ d(i-1)
      !print*,f(i)
-     f(i)=f(i) - (e * f(i-1))/ d(i-1)
+     f(i)=f(i) - (e(i-1) * f(i-1))/ d(i-1)
      !print *, (i-1)*StepSize,d(i), f(i),f(i-1),d(i-1)
   end do
 
@@ -33,7 +42,7 @@ program GaussElimination
   u(NumGridPoints) = f(NumGridPoints)/d(NumGridPoints)
 
   do i=NumGridPoints-1, 2, -1
-     u(i)= (f(i) - e * u(i+1) )/ d(i)
+     u(i)= (f(i) - e(i-1) * u(i+1) )/ d(i)
  !    print*,i
   end do
 
